92ed6fb7ae8adc4c638e7c1bd0a822b1dbdad029
jeel-butani <butanijeel1@gmail.com>
2024-02-09 00:31:33 +0530

    First Commit

---
+++ family_management/lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Home')),
+        body: Center(
+          child: Column(
+            children: [
+              Text("Hello World"),
+            ],
+          ),
+        ));
+  }
+}
